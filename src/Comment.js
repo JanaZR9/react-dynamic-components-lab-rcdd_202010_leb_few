f5a2049874130a5b12df5da1ccb63622c8573dae
Jana Zrayka <janjoune.97@hotmail.com>
2021-01-16 09:55:54 +0000

    Automatically backed up by Learn

--- src/Comment.js
+++ src/Comment.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { Component } from 'react'
 
 export default class Comment extends Component {
@@ -9,3 +10,19 @@ export default class Comment extends Component {
     )
   }
 }
+=======
+//your code here
+import React, { Component } from 'react';
+
+class Comment extends Component {
+  render(){
+    return (
+      <div className="comment">
+      {this.props.commentText};
+      </div>
+      )
+  }
+}
+
+export default Comment
+>>>>>>> 21bb001ce0d0d82968b26cd3b251dc22c7cf873b
